e7bbe86673b3c83b4ecbd59a585ea646c03dde9b
이규행 <2kyuhang@naver.com>
2023-07-01 21:31:52 +0900

    init commit

---
+++ src/main/java/com/sparta/jpaadvance/JpaAdbanceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdbanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAdbanceApplication.class, args);
+    }
+
+}
