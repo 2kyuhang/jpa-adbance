2d8ca4e114a6e3d93a6b4696989ee097d7443d53
이규행 <2kyuhang@naver.com>
2023-07-03 11:09:07 +0900

    @OneToOne 양방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -1,3 +1,5 @@
+//단방향
+/*
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
@@ -13,4 +15,31 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+}*/
+
+//양방향
+package com.sparta.jpaadvance.entity;
+
+import com.sparta.jpaadvance.entity.Food;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) {
+        this.food = food;
+        food.setUser(this);
+    }
 }
