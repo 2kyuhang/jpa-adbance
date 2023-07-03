6c68d496adc984ba93654845f64b859cc1f19dc9
이규행 <2kyuhang@naver.com>
2023-07-03 14:15:57 +0900

    @ManyToMany 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -44,6 +44,7 @@ public class Food {
     private User user;
 }*/
 
+/*
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
@@ -66,5 +67,33 @@ public class Food {
 
     @OneToMany//너는 OneToMany이고 User에 food_id 외래키 넣고 사용할거구나~!
     @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    private List<User> userList = new ArrayList<>();
+}*/
+
+
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    @ManyToMany
+    @JoinTable(name = "orders", // 중간 테이블 생성
+            joinColumns = @JoinColumn(name = "food_id"), // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
+            inverseJoinColumns = @JoinColumn(name = "user_id")) // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
     private List<User> userList = new ArrayList<>();
 }
